d9b055258b0b73b2f1e159cb1f4cf3af49863d8c
aooooooo1 <pdqovvarxx@naver.com>
2023-10-09 20:34:40 +0900

    1
    1

--- practice/jsFolder/async/gym.js
+++ practice/jsFolder/async/gym.js
@@ -21,9 +21,7 @@ helloClass.classList.add('orange');
 helloClass.classList.remove('orange');
 //9.html요소 만들기-ul자식에 새로운li를 추가하기
 let liNew = document.createElement('li');
-liNew.innerHTML = '윗몸일으키기';
-let UlTag = document.querySelector('.list')
-UlTag.appendChild(liNew);
+
 //10.인풋텍스트를 적고 추가를 누르면 리스트에 추가되는 코드 만들기
 //12.추가버튼을 누르면 생성리스트옆에 x버튼도 같이 생성하기 ->10번과 함께
 let btnTag = document.querySelector('#btn');
@@ -53,7 +51,7 @@ removeBtn.addEventListener('click',function(event){
     event.target.parentNode.remove();
 })
 
-//12.과일리스트를 클릭하면 밑에 사진이 나오게하기
+//12.과일리스트를 클릭하면 밑에 사진이 나오게하기,제목도 바뀌기
 let listBtn = document.getElementsByClassName('listBtn');//주의:배열이다. 
 let img = document.getElementById('img');
 let h2 = document.querySelector('.img>h2');
@@ -68,8 +66,114 @@ for(let i = 0; i<listBtn.length; i++){
 let body = document.querySelector('html');
 let circle = document.querySelector('.circle');
 body.addEventListener('mousemove',function(e){
-    console.log(e.clientX,e.clientY)//현제화면에서 마우스 위치
-    console.log(e.pageX,e.pageY)//전체화면에서 마우스 위치
+    //console.log(e.clientX,e.clientY)//현제화면에서 마우스 위치
+    //console.log(e.pageX,e.pageY)//전체화면에서 마우스 위치
     circle.style.left = (e.pageX)+'px';
     circle.style.top = (e.pageY+10)+'px';
 })
+
+document.addEventListener('click', function(event) {
+    const container = document.createElement('div');
+    container.classList.add('fireC');
+    document.body.appendChild(container);
+
+    const numFireworks = 10; // 폭죽을 몇 개 생성할지 조절할 수 있습니다.
+
+    for (let i = 0; i < numFireworks; i++) {
+        const firework = document.createElement('div');
+        firework.classList.add('firework');
+        container.appendChild(firework);
+
+        // 랜덤한 배경색 선택
+        const randomColor = getRandomColor();
+        firework.style.backgroundColor = randomColor;
+        // 클릭한 위치를 기준으로 랜덤한 위치에 폭죽 효과 표시
+        const x = event.clientX + getRandomInt(-50, 50);
+        const y = event.clientY + getRandomInt(-50, 50);
+        firework.style.left = x + 'px';
+        firework.style.top = y + 'px';
+
+        // 1초 후에 요소 제거
+        setTimeout(function() {
+            firework.remove();
+            if (container.childNodes.length === 0) {
+                container.remove();
+            }
+        }, 1000);
+    }
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Q1) submit이벤트가 form태그 안에서 발동되는 조건은?
+
+
+
+//10.8일) object타입(참조타입)을 만들고 한번에 key,value출력해봐라 2가지 방법으로.
+
+const person ={
+    name:'kim',
+    age:28,
+    isprogramer:true,  
+    say(){
+        console.log('hi');
+    },
+};
+//객체접근방식 2가지를 설명해라
+
+console.log(`person.name : ${person.name}`);
+console.log(`person[''] : ${person['age']}`);
+
+let perAry = Object.keys(person); //key[]
+for(let i = 0; i < perAry.length; i++){
+    console.log(`key : ${perAry[i]}`);
+    let key = perAry[i];
+    let value = person[key];
+    console.log(`value : ${value}`);
+}
+for(let k in person){
+    console.log(person[k]);
+}
+
+//배열은 리터럴 , 생성자 표기법이 있다.
+//Q2) 리터럴 배열을 생성하고 forEach를 써서 값을 두개를써서 콘솔에 나타내라,for of 를 사용해라
+
+const ary = ['apple','tangerine','orange','pineapple'];
+ary.pop();
+ary.forEach(  function(item,index){console.log(item,index)}  )
+for(let i of ary){
+    console.log(`========forof ${i}`);
+}
+//Q3) rest parameter을 활용해서 함수값이 몇개가 들어가든 콘솔에 출력하도록 만들어라
+  
+function ary2 (...aryvalue){
+    aryvalue.forEach(function(item,index){
+        console.log(`item : ${item} , index ${index}`);
+    })
+}
+ary2('a','b','aa','asdf');
+
+//Q4) 깊은복사하는 후 값을 하나 변경해봐라
+
+let ary3 = ['q','w','e'];
+let ary4 = [...ary3];
+ary4[0]='qqqq';
+console.log(ary4);
+
+//Q6) return값을 가지는 함수를 생성하고 출력해라
+
+function sum(a,b){
+    return a + b;
+};
+let sum2 = sum(1,2);
+console.log(sum2);
